d25da5b0f2c562d749d3e61932320007d235288a
Jon Doe <joni@email.com>
2023-09-11 11:59:01 +0300

    Init commit with SP4 support
    Signed-off-by: Adam Rozman <adam.rozman@est.tech>

---
+++ test_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run -ti  \
+-v "/home/adam/test/sles_ipa_2022-10-19-1:/ipa_builder" \
+-v "/tmp:/tmp" \
+-v "/home/adam/sles_images:/root/sles_images" \
+quay.io/centos/centos:8 bin/bash
